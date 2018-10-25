64b7daad529ea1f26ed269998f27ff1d2ee221d2
Igor Baiborodine <igor.baiborodine@yandex.com>
2018-10-24 20:14:54 -0400

    Initial commit

---
+++ src/main/java/com/kiroule/campsitebooking/CampsiteBookingApplication.java
@@ -0,0 +1,12 @@
+package com.kiroule.campsitebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampsiteBookingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CampsiteBookingApplication.class, args);
+  }
+}
