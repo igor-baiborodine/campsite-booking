ee2ed6818e0fa06628a9b073a4c26c5d52b37796
Igor Baiborodine <igor.baiborodine@yandex.com>
2020-11-06 11:55:47 -0500

    Fix errors and failures in integration tests

--- src/test/java/com/kiroule/campsitebooking/repository/BookingRepositoryTestIT.java
+++ src/test/java/com/kiroule/campsitebooking/repository/BookingRepositoryTestIT.java
@@ -36,6 +36,41 @@ public class BookingRepositoryTestIT extends TestHelper {
   }
 
   @Test
+  public void findById_savedBooking_savedBookingFound() {
+    // given
+    Booking savedBooking = bookingRepository.save(buildBooking(
+        LocalDate.now().plusDays(1), LocalDate.now().plusDays(2)));
+    // when
+    Optional<Booking> foundBooking = bookingRepository.findById(savedBooking.getId());
+    // then
+    assertThat(foundBooking).hasValue(savedBooking);
+  }
+
+  @Test
+  public void findForDateRange_bookingDatesBeforeRangeStartDate_noBookingFound() {
+    // given: -S-E|-|----|-|--
+    Booking savedBooking = bookingRepository.save(buildBooking(
+        LocalDate.now().plusDays(1), LocalDate.now().plusDays(2)));
+    // when
+    List<Booking> bookings = bookingRepository.findForDateRange(
+        LocalDate.now().plusDays(3), LocalDate.now().plusDays(4));
+    // then
+    assertThat(bookings).isEmpty();
+  }
+
+  @Test
+  public void findForDateRange__bookingStartBeforeRangeStartDateAndBookingEndDateEqualsToRangeStartDate_noBookingFound() {
+    // given: -S|E|----|-|--
+    Booking savedBooking = bookingRepository.save(buildBooking(
+        LocalDate.now().plusDays(1), LocalDate.now().plusDays(2)));
+    // when
+    List<Booking> bookings = bookingRepository.findForDateRange(
+        LocalDate.now().plusDays(2), LocalDate.now().plusDays(3));
+    // then
+    assertThat(bookings).isEmpty();
+  }
+
+  @Test
   public void findForDateRange_bookingStartDateBeforeRangeStartDateAndBookingEndDateWithinRangeDates_bookingFound() {
     // given: -S|-|E---|-|--
     Booking savedBooking = bookingRepository.save(buildBooking(
