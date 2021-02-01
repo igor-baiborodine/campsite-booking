040af604f93984e2d8a0bce58b4e40f769c3bb40
Igor Baiborodine <igor.baiborodine@yandex.com>
2021-02-01 10:59:42 -0500

    Fix docker compose communication failure (#44)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ -n "$WAIT_FOR_DB" ]]; then
+  echo "Sleeping for $WAIT_FOR_DB seconds while the database is being initialized..."
+  sleep "$WAIT_FOR_DB"
+fi
+
 if [[ "$3" == java* && "$(id -u)" = '0' ]]; then
   echo "Switching user from root to $APP_USER..."
   chown -R "$APP_USER:$APP_GROUP" "$APP_HOME"
