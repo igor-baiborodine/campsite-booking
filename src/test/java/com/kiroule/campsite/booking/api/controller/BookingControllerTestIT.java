f912de80b05130c7959304c7778d1afebf7c5148
Igor Baiborodine <igor@kiroule.com>
2022-07-15 15:59:59 -0400

    Upgrade dependencies

--- src/test/java/com/kiroule/campsite/booking/api/controller/BookingControllerTestIT.java
+++ src/test/java/com/kiroule/campsite/booking/api/controller/BookingControllerTestIT.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
