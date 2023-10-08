fd07c9415b4bb505e74d60a47b36ab050c624cee
igor-baiborodine <igor@kiroule.com>
2023-10-08 18:39:19 -0400

    rework MysqlCustomRepositoryContextImplTest.java

--- src/test/java/com/kiroule/campsite/booking/api/repository/context/MysqlCustomRepositoryContextImplTest.java
+++ src/test/java/com/kiroule/campsite/booking/api/repository/context/MysqlCustomRepositoryContextImplTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.verify;
 
 import jakarta.persistence.EntityManager;
 import org.hibernate.query.NativeQuery;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,53 +22,25 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class MysqlCustomRepositoryContextImplTest {
 
-  @Mock NativeQuery query;
+  @Mock NativeQuery<?> query;
 
   @Mock EntityManager entityManager;
 
   @InjectMocks MysqlCustomRepositoryContextImpl classUnderTest;
-
-  Long timeout;
-  Integer count;
-
-  @BeforeEach
-  void beforeEach() {
-    timeout = null;
-    count = null;
-  }
-
-  private void given_entityManagerCreatesNativeQuery() {
-    doReturn(query).when(entityManager).createNativeQuery(any());
-  }
 
   @Nested
   class SetLockTimeout {
 
     @Test
     void happy_path() {
-      given_timeout(3000L);
-      given_entityManagerCreatesNativeQuery();
-      given_queryExecutesUpdate(1);
-
-      when_setLockTimeout();
-
-      then_assertUpdateExecuted(1);
-    }
-
-    private void given_timeout(long value) {
-      timeout = value;
-    }
-
-    private void given_queryExecutesUpdate(int updatedCount) {
-      doReturn(updatedCount).when(query).executeUpdate();
-    }
-
-    private void when_setLockTimeout() {
-      count = classUnderTest.setLockTimeout(timeout);
-    }
-
-    private void then_assertUpdateExecuted(int expectedCount) {
-      assertThat(count).isEqualTo(expectedCount);
+      // given
+      var timeout = 3000L;
+      doReturn(query).when(entityManager).createNativeQuery(any());
+      doReturn(1).when(query).executeUpdate();
+      // when
+      var count = classUnderTest.setLockTimeout(timeout);
+      // then
+      assertThat(count).isEqualTo(1);
       verify(entityManager).createNativeQuery("set session innodb_lock_wait_timeout = 3");
       verify(query).executeUpdate();
     }
@@ -80,24 +51,13 @@ class MysqlCustomRepositoryContextImplTest {
 
     @Test
     void happy_path() {
-      given_entityManagerCreatesNativeQuery();
-      given_queryReturnsSingleResult(3L); // seconds
-
-      when_getLockTimeout();
-
-      then_assertFetchedTimeout(3000L); // milliseconds
-    }
-
-    private void given_queryReturnsSingleResult(long timeoutInSec) {
-      doReturn(timeoutInSec).when(query).getSingleResult();
-    }
-
-    private void when_getLockTimeout() {
-      timeout = classUnderTest.getLockTimeout();
-    }
-
-    private void then_assertFetchedTimeout(long expectedTimeout) {
-      assertThat(timeout).isEqualTo(expectedTimeout);
+      // given
+      doReturn(query).when(entityManager).createNativeQuery(any());
+      doReturn(3L).when(query).getSingleResult();
+      // when
+      var timeout = classUnderTest.getLockTimeout();
+      // then
+      assertThat(timeout).isEqualTo(3000L);
       verify(entityManager).createNativeQuery("select @@innodb_lock_wait_timeout");
       verify(query).getSingleResult();
     }
